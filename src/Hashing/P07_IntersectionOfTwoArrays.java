c1ce7518cbf50078f86835856cf4d40379db075f
saidaragani <saidaragani@gmail.com>
2026-09-10 10:20:26 +0530

    DAY-3

--- src/Hashing/P07_IntersectionOfTwoArrays.java
+++ src/Hashing/P07_IntersectionOfTwoArrays.java
@@ -1,7 +1,34 @@
 package Hashing;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 public class P07_IntersectionOfTwoArrays {
+
     public static void main(String[] args){
+        int[] nums1 = {4,9,5};
+        int[] nums2 = {9,4,9,8,4};
+        HashSet<Integer> set1 = new HashSet<>();
+        HashSet<Integer> result = new HashSet<>();
+
+        for (int x : nums1) {
+            set1.add(x);
+        }
+
+        for (int x : nums2) {
+            if (set1.contains(x)) {
+                result.add(x);
+            }
+        }
+
+        int[] ans = new int[result.size()];
+        int i = 0;
+
+        for (int x : result) {
+            ans[i++] = x;
+        }
+
+        System.out.println(Arrays.toString(ans));
 
     }
 }
--- src/Hashing/P08_LongestConsecutiveSequence.java
+++ src/Hashing/P08_LongestConsecutiveSequence.java
@@ -1,7 +1,51 @@
 package Hashing;
 
+import java.util.HashSet;
+
 public class P08_LongestConsecutiveSequence {
+
+    public int longestconsec(int[] nums){
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        HashSet<Integer> set = new HashSet<>();
+
+        // Store all unique numbers
+        for (int x : nums) {
+            set.add(x);
+        }
+
+        int maxCount = 0;
+
+        for (int x : set) {
+
+            // x is the start of a sequence
+            if (!set.contains(x - 1)) {
+
+                int current = x;
+                int count = 1;
+
+                // Count consecutive numbers
+                while (set.contains(current + 1)) {
+                    current++;
+                    count++;
+                }
+
+                if (count > maxCount) {
+                    maxCount = count;
+                }
+            }
+        }
+
+        return maxCount;
+    }
+
     public static void main(String[] args){
+        int[] nums = {1};
+        P08_LongestConsecutiveSequence obj = new P08_LongestConsecutiveSequence();
+        int res = obj.longestconsec(nums);
+        System.out.println(res);
 
     }
 }
